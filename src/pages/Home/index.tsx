3a682e30c3b03328e39bdfb76a7049de4636841b
Henrique Lopes <henriquehendell@gmail.com>
2021-03-24 23:58:27 -0300

    feat: pages and navigation

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const Home: React.FC = () => {
+  return (
+      <View>
+          <Text style={{color: '#000'}} >
+                Home
+          </Text>
+      </View>
+  );
+}
+
+export default Home;
